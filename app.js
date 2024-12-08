4ae1bfe3856b3e0b24096497bcb01b36f5cad3c7
ani2646 <akshatgoyal2646@gmail.com>
2024-12-08 23:37:49 +0000

    final changes

--- app.js
+++ app.js
@@ -10,12 +10,28 @@ let todos = [];
       }
       }
 
+      function removeTodo(index) {
+        todos.splice(index, 1); // Remove the todo at the specified index
+        renderTodos(); // Re-render the todo list
+      }
+
       function renderTodos() {
-      const list = document.getElementById('todo-list');
-      list.innerHTML = '';
-      todos.forEach((todo, index) => {
-         const listItem = document.createElement('li');
-         listItem.textContent = todo;
-         list.appendChild(listItem);
-      });
+        const list = document.getElementById('todo-list');
+        list.innerHTML = '';
+        todos.forEach((todo, index) => {
+          const listItem = document.createElement('li');
+          listItem.textContent = todo;
+      
+          // Create a remove button for each todo
+          const removeBtn = document.createElement('button');
+          removeBtn.textContent = 'Delete';
+          removeBtn.style.marginLeft = '10px';
+          removeBtn.style.color = 'red';
+          removeBtn.onclick = function () {
+            removeTodo(index);
+          }; // Attach click handler to remove todo
+          listItem.appendChild(removeBtn);
+      
+          list.appendChild(listItem);
+        });
       }
